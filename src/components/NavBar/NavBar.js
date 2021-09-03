37450a9725fb048f38c89f78387c49472c637aa8
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-09-03 10:34:02 -0500

    added 'Home' href to the nav element

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -23,7 +23,7 @@ function NavBar() {
 				<div className="collapse navbar-collapse" id="navbarColor01">
 					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
 						<li className="nav-item">
-							<a className="nav-link active" aria-current="page" href="#">
+							<a className="nav-link active" aria-current="page" href="/home">
 								Home
 							</a>
 						</li>
