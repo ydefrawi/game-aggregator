ef517684d9ea620b06a54cf3e7ec326772c1479f
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-10-27 15:47:01 -0500

    got rid of favorites in User model

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,26 +1,31 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require("../config/connection")
+const sequelize = require('../config/connection');
 
-class User extends Model{}
+class User extends Model {}
 
 //takes in 2 arguments, fields and options
-User.init({
-    firstName:{
-        type:DataTypes.STRING,
-        allowNull:false
-    },
-    lastName:{
-        type:DataTypes.STRING,
-        allowNull:false
-    },
-    favorites:{
-        type:DataTypes.STRING,
-        allowNull:true
-    }}, {
-        sequelize, 
-        timestamps:false,
-        modelName:'User'
-    }
-)
+User.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		firstName: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		lastName: {
+			type: DataTypes.STRING,
+			allowNull: false
+		}
+	},
+	{
+		sequelize,
+		timestamps: false,
+		modelName: 'User'
+	}
+);
 
-module.exports=User;
+module.exports = User;
