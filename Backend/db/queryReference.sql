a853f34b75e9a7892e20a8d6e793369e2cb55941
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-10-28 17:05:37 -0500

    added query reference

--- Backend/db/queryReference.sql
+++ Backend/db/queryReference.sql
@@ -1,7 +1,12 @@
-
-
 SELECT * 
 FROM gamehub_db.favorites 
 left join games on games.id=favorites.game_id
 WHERE user_id=2
 
+
+-- 2 joins, shows user names and game names
+SELECT favorites.id, favorites.user_id, games.name as "Game Name", users.lastName as "Last Name" 
+FROM gamehub_db.favorites 
+left join games on games.id=favorites.game_id
+left join users on users.id=favorites.user_id
+WHERE user_id=2
